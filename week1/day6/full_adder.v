3e1d4f6d2b3ba7d4ea4e73b83808c142ab9c00b1
simon9721 <shwang17@hawk.iit.edu>
2025-02-04 12:27:10 -0600

    added day7 README & completed full_adder.v

--- week1/day6/full_adder.v
+++ week1/day6/full_adder.v
@@ -10,3 +10,19 @@ assign sum = a ^ b;       // XOR operation for Sum
 assign carry = a & b;     // AND operation for Carry
 
 endmodule
+
+module full_adder (
+	input wire A,		// Input A 
+	input wire B		// Input B 
+	input wire Cin		// Input CarryIn
+	output wire S 		// Sum output
+	output wire Cout	// CarryOut output
+);
+
+// Logic implementation
+	wire S1, C1, C2;
+	
+	half_adder HA1 (.a(A), .b(B), .sum(S1), .carry(C1));
+	half_adder HA2 (.a(S1), .b(Cin), .sum(S), .carry(C2));
+	assign Cout = C1 | C2;
+endmodule
